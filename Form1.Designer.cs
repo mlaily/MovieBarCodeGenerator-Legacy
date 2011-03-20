207767269b4f730728dc661deef41f39b35a1d82
yaurthek <yaurthek@localhost>
2011-03-20 22:37:26 +0100

    first release

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,43 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtPath = new System.Windows.Forms.TextBox();
-            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtPathIn = new System.Windows.Forms.TextBox();
+            this.btnBrowseIn = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.btnProcess = new System.Windows.Forms.Button();
             this.ofd1 = new System.Windows.Forms.OpenFileDialog();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnBrowseOut = new System.Windows.Forms.Button();
+            this.txtPathOut = new System.Windows.Forms.TextBox();
+            this.sfd1 = new System.Windows.Forms.SaveFileDialog();
+            this.link1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
-            // txtPath
+            // txtPathIn
             // 
-            this.txtPath.Location = new System.Drawing.Point(12, 31);
-            this.txtPath.Name = "txtPath";
-            this.txtPath.Size = new System.Drawing.Size(298, 20);
-            this.txtPath.TabIndex = 0;
+            this.txtPathIn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPathIn.Location = new System.Drawing.Point(12, 31);
+            this.txtPathIn.Name = "txtPathIn";
+            this.txtPathIn.Size = new System.Drawing.Size(295, 20);
+            this.txtPathIn.TabIndex = 0;
             // 
-            // btnBrowse
+            // btnBrowseIn
             // 
-            this.btnBrowse.Location = new System.Drawing.Point(316, 29);
-            this.btnBrowse.Name = "btnBrowse";
-            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
-            this.btnBrowse.TabIndex = 1;
-            this.btnBrowse.Text = "Browse...";
-            this.btnBrowse.UseVisualStyleBackColor = true;
-            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            this.btnBrowseIn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowseIn.Location = new System.Drawing.Point(313, 29);
+            this.btnBrowseIn.Name = "btnBrowseIn";
+            this.btnBrowseIn.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseIn.TabIndex = 1;
+            this.btnBrowseIn.Text = "Browse...";
+            this.btnBrowseIn.UseVisualStyleBackColor = true;
+            this.btnBrowseIn.Click += new System.EventHandler(this.btnBrowse_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.Size = new System.Drawing.Size(64, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Video file :";
+            this.label1.Text = "Video path :";
             // 
             // btnProcess
             // 
-            this.btnProcess.Location = new System.Drawing.Point(12, 86);
+            this.btnProcess.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnProcess.Location = new System.Drawing.Point(313, 130);
             this.btnProcess.Name = "btnProcess";
             this.btnProcess.Size = new System.Drawing.Size(75, 23);
             this.btnProcess.TabIndex = 3;
@@ -72,27 +85,122 @@
             this.btnProcess.UseVisualStyleBackColor = true;
             this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
             // 
+            // ofd1
+            // 
+            this.ofd1.Filter = "Avi files|*.avi|Wmv files|*.wmv|All files|*.*";
+            // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 57);
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 159);
             this.progressBar1.Maximum = 1000;
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(379, 23);
+            this.progressBar1.Size = new System.Drawing.Size(376, 23);
             this.progressBar1.Step = 1;
             this.progressBar1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Image size :";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(12, 122);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(55, 20);
+            this.txtWidth.TabIndex = 7;
+            this.txtWidth.Text = "1000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "x";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(81, 122);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(55, 20);
+            this.txtHeight.TabIndex = 9;
+            this.txtHeight.Text = "500";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Output path :";
+            // 
+            // btnBrowseOut
+            // 
+            this.btnBrowseOut.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowseOut.Location = new System.Drawing.Point(313, 71);
+            this.btnBrowseOut.Name = "btnBrowseOut";
+            this.btnBrowseOut.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseOut.TabIndex = 11;
+            this.btnBrowseOut.Text = "Browse...";
+            this.btnBrowseOut.UseVisualStyleBackColor = true;
+            this.btnBrowseOut.Click += new System.EventHandler(this.btnBrowseOut_Click);
+            // 
+            // txtPathOut
+            // 
+            this.txtPathOut.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPathOut.Location = new System.Drawing.Point(12, 73);
+            this.txtPathOut.Name = "txtPathOut";
+            this.txtPathOut.Size = new System.Drawing.Size(295, 20);
+            this.txtPathOut.TabIndex = 10;
+            // 
+            // sfd1
+            // 
+            this.sfd1.Filter = "Bitmap|*.bmp|Jpeg|*.jpg|Png|*.png|Gif|*.gif|All files|*.*";
+            // 
+            // link1
+            // 
+            this.link1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.link1.AutoSize = true;
+            this.link1.LinkColor = System.Drawing.Color.SteelBlue;
+            this.link1.Location = new System.Drawing.Point(12, 189);
+            this.link1.Name = "link1";
+            this.link1.Size = new System.Drawing.Size(101, 13);
+            this.link1.TabIndex = 13;
+            this.link1.TabStop = true;
+            this.link1.Text = "ArcaneSanctum.net";
+            this.link1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.link1_LinkClicked);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(651, 404);
+            this.ClientSize = new System.Drawing.Size(400, 211);
+            this.Controls.Add(this.link1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnBrowseOut);
+            this.Controls.Add(this.txtPathOut);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.btnProcess);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnBrowse);
-            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnBrowseIn);
+            this.Controls.Add(this.txtPathIn);
+            this.MinimumSize = new System.Drawing.Size(416, 249);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Movie BarCode Generator - ";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,12 +208,21 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtPath;
-        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtPathIn;
+        private System.Windows.Forms.Button btnBrowseIn;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnProcess;
         private System.Windows.Forms.OpenFileDialog ofd1;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnBrowseOut;
+        private System.Windows.Forms.TextBox txtPathOut;
+        private System.Windows.Forms.SaveFileDialog sfd1;
+        private System.Windows.Forms.LinkLabel link1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,43 +14,121 @@ namespace MovieBarCode
         public Form1()
         {
             InitializeComponent();
+            this.Text += Application.ProductVersion;
         }
 
         private void btnBrowse_Click(object sender, EventArgs e)
         {
             if (ofd1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                txtPath.Text = ofd1.FileName;
+                txtPathIn.Text = ofd1.FileName;
+            }
+        }
+
+
+        private void btnBrowseOut_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(ofd1.FileName))
+            {
+                sfd1.FileName = System.IO.Path.GetFileNameWithoutExtension(ofd1.FileName) + ".png";
+            }
+            if (sfd1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtPathOut.Text = sfd1.FileName;
             }
         }
 
         private void btnProcess_Click(object sender, EventArgs e)
         {
-            GenerateMovieBarCode(txtPath.Text);
+            if (System.IO.File.Exists(txtPathOut.Text))
+            {
+                if (MessageBox.Show(string.Format(@"The file '{0}' already exists. do you want to overwrite it?",txtPathOut.Text),"Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != System.Windows.Forms.DialogResult.Yes)
+                {
+                    return;
+                }
+            }
+            try
+            {
+                var fs = new System.IO.FileStream(txtPathOut.Text, System.IO.FileMode.Create);
+                fs.Dispose();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid output path!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (System.IO.File.Exists(txtPathIn.Text))
+            {
+                int width;
+                int height;
+                if (int.TryParse(txtWidth.Text, out width) && int.TryParse(txtHeight.Text, out height))
+                {
+                    this.Enabled = false;
+                    GenerateMovieBarCode(txtPathIn.Text, width, height,txtPathOut.Text);
+                    this.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("The input values must be valid integers!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("File not found!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
         }
 
-        private void GenerateMovieBarCode(string videoPath)
+        private void GenerateMovieBarCode(string videoPath, int width, int height, string outputPath)
         {
             progressBar1.Value = progressBar1.Minimum;
+            progressBar1.Maximum = width;
             VideoHelper v = new VideoHelper(videoPath);
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    v.SaveFrameFromVideo(((double)i) / 100.0, string.Format(@"C:\x{0}.bmp", i));
-            //}
-            Bitmap b = new Bitmap(1000,500);
+            Bitmap b = new Bitmap(width, height);
             System.Drawing.Graphics g = Graphics.FromImage(b);
-            for (int i = 0; i <= 1000; i++)
+            for (int i = 0; i < width; i++)
             {
                 //v.GetFrameFromVideo(((double)i) / 100.0).Save(string.Format(@"C:\x{0}.jpg", i), System.Drawing.Imaging.ImageFormat.Jpeg);
-                Bitmap tempB = v.GetFrameFromVideo(((double)i) / 1000.0);
-                g.DrawImage(tempB, i, 0, 1, 500);
+                Bitmap tempB = v.GetFrameFromVideo(((double)i) / (double)width);
+                g.DrawImage(tempB, i, 0, 1, height);
                 progressBar1.PerformStep();
                 Application.DoEvents();
             }
-            b.Save(string.Format(@"C:\{0}.png",System.IO.Path.GetFileNameWithoutExtension(txtPath.Text).Trim(".".ToCharArray())));
+            System.Drawing.Imaging.ImageFormat format;
+            switch (System.IO.Path.GetExtension(outputPath).Trim(".".ToCharArray()).ToLowerInvariant())
+            {
+                case "bmp":
+                    format = System.Drawing.Imaging.ImageFormat.Bmp;
+                    break;
+                case "jpg":
+                case "jpeg":
+                    format = System.Drawing.Imaging.ImageFormat.Jpeg;
+                    break;
+                case "gif":
+                    format = System.Drawing.Imaging.ImageFormat.Gif;
+                    break;
+                case "png":
+                    format = System.Drawing.Imaging.ImageFormat.Png;
+                    break;
+                default:
+                    format = System.Drawing.Imaging.ImageFormat.Png;
+                    break;
+            }
+            b.Save(outputPath, format);
             v.Dispose();
         }
 
+        private void link1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Threading.Thread t = new System.Threading.Thread(new System.Threading.ThreadStart(ThreadedStart));
+            t.Start();
+        }
+
+        private void ThreadedStart()
+        {
+            System.Diagnostics.Process.Start(@"http://arcanesanctum.net");
+        }
 
     }
 }
